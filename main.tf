952a6c078fea7814f624ef4dc02a29c537bb1a65
Aswanidevm <maswanidev@gmail.com>
2023-08-08 12:11:50 +0530

    zdfgvb

--- main.tf
+++ main.tf
@@ -12,11 +12,17 @@ variable "paramter"{
   { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.myprojecdevops.info:8080/", type = "String" },
   { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.myprojecdevops.info:8080/", type = "String" },
   { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.myprojecdevops.info:8080/", type = "String" },
+  
+  
   { name = "roboshop.dev.cart.redis_host", value = "redis-dev.myprojecdevops.info", type = "String" },
   { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.myprojecdevops.info", type = "String" },
   { name = "roboshop.dev.cart.catalogue_port", value = "8080", type = "String" },
+  
+  
   { name = "roboshop.dev.catalogue.mongo", value = "true", type = "String" },
   { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb-dev.myprojecdevops.info", type = "String" },
+  
+  
   { name = "roboshop.dev.payment.cart_host", value = "cart-dev.myprojecdevops.info", type = "String" },
   { name = "roboshop.dev.payment.cart_port", value = "8080", type = "String" },
   { name = "roboshop.dev.payment.user_host", value = "user-dev.myprojecdevops.info", type = "String" },
@@ -24,5 +30,27 @@ variable "paramter"{
   { name = "roboshop.dev.payment.amqp_host", value = "rabbitmq-dev.myprojecdevops.info", type = "String" },
   { name = "roboshop.dev.payment.amqp_user", value = "8080", type = "String" },
   { name = "roboshop.dev.payment.amqp_pass", value = "8080", type = "String" },
+   
+   
+    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.myprojecdevops.info:8080", type = "String" },
+    { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.myprojecdevops.info", type = "String" },
+    { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
+    { name = "roboshop.dev.user.redis_host", value = "redis-dev.myprojecdevops.info", type = "String" },
+    //    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.rdevopsb73.online:27017/users", type = "String" },
+    { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
+    
+     ### Passwords will be never part of git repos, Usually in organizations we create them nanually. Meaning whom ever have access they will provision these secrets in parameter store.
+    { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.mysql.password", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.docdb.password", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
+    { name = "roboshop.dev.catalogue.docdb_endpoint", value = "docdb-dev.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com", type = "String" },
+    { name = "roboshop.dev.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
+    { name = "roboshop.dev.user.docdb_endpoint", value = "docdb-dev.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com", type = "String" },
+
+
     ]
 }
