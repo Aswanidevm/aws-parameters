a5a44ba03fc50099f70f921c9181195b10a950df
Aswanidevm <maswanidev@gmail.com>
2023-08-09 07:12:24 +0530

    asf

--- main.tf
+++ main.tf
@@ -31,20 +31,24 @@ variable "paramter"{
   { name = "roboshop.dev.payment.amqp_user", value = "8080", type = "String" },
    
    
-    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.myprojecdevops.info:8080", type = "String" },
-    { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.myprojecdevops.info", type = "String" },
-    { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
-    { name = "roboshop.dev.user.redis_host", value = "redis-dev.myprojecdevops.info", type = "String" },
+  { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.myprojecdevops.info:8080", type = "String" },
+  { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.myprojecdevops.info", type = "String" },
+  { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
+  { name = "roboshop.dev.user.redis_host", value = "redis-dev.myprojecdevops.info", type = "String" },
+  { name = "roboshop.dev.dispatch.amqp_host", value = "rabbitmq-dev.myprojecdevops.info", type = "String" },
+
     //    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.rdevopsb73.online:27017/users", type = "String" },
-    { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
-    { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
-    { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
+  { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
+  { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
+  { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
+  { name = "roboshop.dev.dispatch.amqp_user", value = "roboshop", type = "String" },
     
-     ### Passwords will be never part of git repos, Usually in organizations we create them nanually. Meaning whom ever have access they will provision these secrets in parameter store.
-    { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
-    { name = "roboshop.dev.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString" },
-    { name = "roboshop.dev.mysql.password", value = "roboshop123", type = "SecureString" },
-    { name = "roboshop.dev.docdb.password", value = "roboshop123", type = "SecureString" },
+     ### Passwords will be never part of git repos, Usually in organizations we create them nanually.
+  { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
+  { name = "roboshop.dev.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString" },
+  { name = "roboshop.dev.dispatch.amqp_pass", value = "roboshop123", type = "SecureString" },
+  { name = "roboshop.dev.mysql.password", value = "roboshop123", type = "SecureString" },
+  { name = "roboshop.dev.docdb.password", value = "roboshop123", type = "SecureString" },
     # { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
     # { name = "roboshop.dev.catalogue.docdb_endpoint", value = "docdb-dev.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com", type = "String" },
     # { name = "roboshop.dev.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
