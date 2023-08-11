6ffb7ae5f0d34244749c7a080f3c25c64c765638
Aswanidevm <maswanidev@gmail.com>
2023-08-11 11:39:10 +0530

    zc

--- main.tf
+++ main.tf
@@ -36,8 +36,7 @@ variable "paramter"{
   { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
   { name = "roboshop.dev.user.redis_host", value = "redis-dev.myprojecdevops.info", type = "String" },
   { name = "roboshop.dev.dispatch.amqp_host", value = "rabbitmq-dev.myprojecdevops.info", type = "String" },
-
-    //    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.rdevopsb73.online:27017/users", type = "String" },
+  { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.myprojecdevops.info", type = "String" },
   { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
   { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
   { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
